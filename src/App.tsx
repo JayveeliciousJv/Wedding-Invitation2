d32ef06ac0cafaffd0758484e7854585dd7c394c
JayveeliciousJv <61629268+JayveeliciousJv@users.noreply.github.com>
2026-04-28 13:30:55 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,15 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+
+      {/* ✅ FIX IS HERE */}
+      <BrowserRouter basename="/Wedding-Invitation2">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
